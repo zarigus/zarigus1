35817aecf24bab25ffed53d1f221f6cde7a796c4
Johan zarigus1 <zarigus1@zarigus.com>
2013-10-21 11:49:45 +0000

    add 2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World add1!');
+  response.send('Hello World add 2 !');
 });
 
 var port = process.env.PORT || 5000;
